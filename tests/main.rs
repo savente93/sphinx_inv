5905d13f81871812439f0391fc02d8526f5e809f
Sam Vente <savente93@proton.me>
2026-03-23 12:16:42 +0100

    fix: remove color_eyre from integration test

--- tests/main.rs
+++ tests/main.rs
@@ -1,14 +1,8 @@
 #[allow(unused_imports)]
 use pretty_assertions::{assert_eq, assert_ne};
 
-use color_eyre::eyre::Result;
 #[allow(unused_imports)]
 use sphinx_inv::*;
-
-#[allow(clippy::missing_errors_doc)]
-pub fn setup() -> Result<()> {
-    Ok(())
-}
 
 #[test]
 fn test_example() {
